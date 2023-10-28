9f8834e7d3000d3ee99367ebb29ef1a1498e1b07
Anil <anil.kumar@shellinfoglobal.com>
2023-10-28 13:23:42 +0530

    Using React from node modules

--- episode-2/App.js
+++ episode-2/App.js
@@ -22,6 +22,9 @@
     </div>
 */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", { id: "heading" }, "Hello world"),
